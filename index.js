b0763da567ca295b2898065513567a1f1d277336
Harsh gaur <harshgaur838@gmail.com>
2024-02-08 15:42:58 +0530

    add some code

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-var express=require('express'); 
-var https=require('https'); 
-var dotenv = require('dotenv'); 
-var bodyParser=require('body-parser'); 
+var express = require("express");
+var https = require("https");
+var dotenv = require("dotenv");
+var bodyParser = require("body-parser");
 
+dotenv.config();
+var app = express();
+app.use(express.static("public"));
 
-dotenv.config()
-var app=express(); 
-app.use(express.static("public")); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.urlencoded({extended:true})); 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/home.html");
+});
 
- 
-app.get('/', function(req,res){
-    res.sendFile(__dirname + "/home.html");
-}) 
+app.post("/", function (req, res) {
+  console.log(req.body);
+  res.send(req.body);
+});
 
-app.post('/', function(req,res){
-    console.log(req.body);
-    res.send(req.body);
-})
- 
+app.get("/about", (req, res) => {
+  res.send("Hello from About us");
+});
 
-app.listen(3000,function(){ 
-    console.log('server started listening on port 3000'); 
-}); 
-
- 
+app.listen(3000, function () {
+  console.log("server started listening on port 3000");
+});
